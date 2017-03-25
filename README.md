d87ab5043a31fd9f96751505d492891f7691209f
Evan Tseng <evan@tseng.io>
2017-03-25 13:51:27 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Air Quality Monitoring Station MQTThook
+A [MQTThook][mqtthook] to store the PM2.5 data from a [handmade air quality monitoring station][air-quality-monitoring-station] in a [Firebase][firebase] database.
+
+[mqtthook]: https://github.com/evanxd/mqtthook
+[air-quality-monitoring-station]: https://github.com/evanxd/air-quality-monitoring-station
+[firebase]: https://firebase.google.com
---
+++ index.js
@@ -0,0 +1,26 @@
+var config = require('./config.json');
+var MQTThook = require('mqtthook');
+var mqtthook = new MQTThook(config.mqtthook.brokerUrl, config.mqtthook.options);
+var firebase = require('firebase-admin');
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(require('./auth.json')),
+  databaseURL: config.databaseURL,
+});
+
+var ref = firebase.database().ref(config.referencePath);
+
+ref.child('info').set({
+  name: config.stationInfo.name,
+});
+
+mqtthook.hook(config.mqtthook.topic).trigger(data => {
+  ref.child('data').push({
+      date: new Date().toUTCString(),
+      pm2_5: data.pm2_5,
+    },
+    error => {
+      config.debug && (error ? console.error(error.message) :
+                               console.log(`PM2.5: ${data.pm2_5}`));
+    });
+});
