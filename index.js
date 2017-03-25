8f7a56d266ebf4121f6f05bfea7695eb1cdc5d74
Evan Tseng <evan@tseng.io>
2017-03-25 15:31:24 +0800

    Refactor it as module for reusing

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-var config = require('./config.json');
+'use strict';
+
 var MQTThook = require('mqtthook');
-var mqtthook = new MQTThook(config.mqtthook.brokerUrl, config.mqtthook.options);
 var firebase = require('firebase-admin');
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(require('./auth.json')),
-  databaseURL: config.databaseURL,
-});
-
-var ref = firebase.database().ref(config.dataReferencePath);
-ref.child('info').set(config.deviceInfo);
-
-mqtthook.hook(config.mqtthook.topic).trigger(data => {
-  // We always save the sensor data's datetime with server datetime.
-  data = filter(data);
-  data.datetime = new Date().toUTCString();
-  ref.child('data').push(data, error => {
-    config.debug && (error ? console.error(error.message) :
-                             console.log(`Data stored: ${JSON.stringify(data)}`));
+function Data2FirebaseMQTThook(params) {
+  this._params = params;
+  firebase.initializeApp({
+    credential: firebase.credential.cert(require('./auth.json')),
+    databaseURL: params.databaseURL,
   });
-});
-
-function filter(_data) {
-  if (!config.dataFilter) {
-    return _data;
-  }
-  var data = {};
-  for (var key in _data) {
-    if (config.dataFilter[key]) {
-      data[key] = _data[key];
-    }
-  }
-  return data;
+  var ref = firebase.database().ref(params.dataReferencePath);
+  ref.child('info').set(params.deviceInfo);
+  this._dataRef = ref;
+  this._mqtthook = new MQTThook(params.mqtthook.brokerUrl, params.mqtthook.options);
 }
+
+Data2FirebaseMQTThook.prototype = {
+  _params: null,
+  _mqtthook: null,
+  _dataRef: null,
+
+  hook: function() {
+    this._mqtthook.hook(this._params.mqtthook.topic).trigger(data => {
+      // We always save the sensor data's datetime with server datetime.
+      data = this._filter(data);
+      data.datetime = new Date().toUTCString();
+      this._dataRef.child('data').push(data, error => {
+        this._params.debug && (error ? console.error(error.message) :
+                                       console.log(`Data stored: ${JSON.stringify(data)}`));
+      });
+    });
+  },
+
+  _filter: function(_data) {
+    if (!this._params.dataFilter) {
+      return _data;
+    }
+    var data = {};
+    for (var key in _data) {
+      if (this._params.dataFilter[key]) {
+        data[key] = _data[key];
+      }
+    }
+    return data;
+  },
+};
+
+module.exports = Data2FirebaseMQTThook;
